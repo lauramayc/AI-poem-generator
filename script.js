17a5f682f24d26d8778d9163da210f261c916960
lauramayc <lauramayc@live.com>
2025-06-24 16:27:18 +0100

    UX
    loading bar added.
    Some features made more asthetic in css

--- script.js
+++ script.js
@@ -17,10 +17,13 @@ function generatePoem(event) {
   let ApiKey = "a6ee8503c8fee7b4f4c3ocbtac96961d";
   let prompt = "Write a poem about " + instructionInput.value;
   let context =
-    "You are a poet. Write a poem based on the user's input. use </br> for new lines in the poem and <br> </br> for a new verse";
+    "You are a poet. Write a poem based on the user's input. use htlm and use </br> for new lines in the poem and for a new verse use <p>";
   let ApiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${ApiKey}`;
 
   axios.get(ApiURL).then(displayPoem);
+  let load = document.querySelector("#poem");
+  load.innerHTML = `Making a poem about ${instructionInput.value}... <div class="loader"></div>`;
+
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
